b2681d8b400965062c8a8550851ebaa8a012b480
SimaGelfman <GelfmanSima@gmail.com>
2017-01-25 23:00:27 +0200

    Change format settings

--- sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
@@ -1,7 +1,7 @@
 package com.gelfman.courseJava.sandbox;
 
-public class MyFirstProject{
-	public static void main(String args[]){
-		System.out.println("Hello from Israel");
+public class MyFirstProject {
+	public static void main ( String args[] ) {
+		System.out.println ( "Hello from Israel" );
 	}
 }
