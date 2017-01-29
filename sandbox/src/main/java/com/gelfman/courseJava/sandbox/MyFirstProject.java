a8c8cb67d4dae82af703877d612a7b02e460e0f1
SimaGelfman <GelfmanSima@gmail.com>
2017-01-29 13:10:09 +0200

    Add class Point

--- sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
@@ -12,6 +12,17 @@ public class MyFirstProject {
 		Rectangle r =  new Rectangle (5.5, 6.5);
 		print("Rectangle area with sides a = " + r.getA () + " and b = "+ r.getB ()  + " is "  + r.area( ));
 
+		Point p1 = new Point ( 0, 2 );
+		Point p2 = new Point ( 1, 1 );
+
+		print ( "Distance between p1: (" + p1.getX () + ", " + p1.getY ()
+									+ ") and p2: (" + p2.getX () + " ," + p2.getY () + ") is " + p1.distance ( p2 ) );
+
+		print ( "Distance between p1: (" + p1.getX () + ", " + p1.getY ()
+						+ ") and p2: (" + p2.getX () + " ," + p2.getY () + ") is " + distance (p1,  p2 ) );
+
+
+
 	}
 
 	public static void print(String something){
@@ -24,5 +35,9 @@ public class MyFirstProject {
 
 	public static double area(double a, double b){
 		return a * b;
+	}
+
+	public static double distance(Point p1, Point p2){
+		return Math.sqrt((p2.getX () - p1.getX ()) * (p2.getX () - p1.getX ()) + (p2.getY () - p1.getY ()) * (p2.getY () - p1.getY ()));
 	}
 }
