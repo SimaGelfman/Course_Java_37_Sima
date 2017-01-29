33bb9ae29c7cf940fa07d106b73f5eec46876fd7
SimaGelfman <GelfmanSima@gmail.com>
2017-01-29 11:53:15 +0200

    Add squareArea and rectangleArea functions

--- sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
+++ sandbox/src/main/java/com/gelfman/courseJava/sandbox/MyFirstProject.java
@@ -2,6 +2,27 @@ package com.gelfman.courseJava.sandbox;
 
 public class MyFirstProject {
 	public static void main ( String args[] ) {
-		System.out.println ( "Hello from Israel" );
+
+		print ( "Hello, friends" );
+
+		double l = 5.5;
+		print("Square area with side " + l  + " is "  + area(l));
+
+		double a = 5.5;
+		double b = 4.5;
+		print("Rectangle area with side " + l  + " is "  + area(a,b));
+
+	}
+
+	public static void print(String something){
+		System.out.println ( something );
+	}
+
+	public static double area(double l){
+		return l * l;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
